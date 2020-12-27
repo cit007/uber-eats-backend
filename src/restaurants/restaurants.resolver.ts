43cfb694a2b82d81b3d071471f67db7cab829c39
cit007 <itcho0077@gmail.com>
2020-12-28 00:42:47 +0900

    test query muataion with many param : not good - please use dto

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
 
 @Resolver((of) => Restaurant)
@@ -6,5 +6,14 @@ export class RestaurantResolver {
   @Query((returns) => [Restaurant])
   myRestaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(
+    @Args('name') name: string,
+    @Args('isVegan') name: boolean,
+    @Args('address') name: string,
+    @Args('ownerName') name: string,
+  ): boolean {
+    return true;
   }
 }
