04a70ef91038513b07d54315cf0704fa4dad5f69
cit007 <itcho0077@gmail.com>
2021-01-01 23:20:02 +0900

    updateRestaurant using partialType

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurants.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -17,6 +18,18 @@ export class RestaurantResolver {
   ): Promise<boolean> {
     try {
       await this.restaurantServce.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  @Mutation((returns) => Boolean)
+  async updateRestaurant(
+    @Args('Input') updateRestaurantDto: UpdateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantServce.updateRestaurant(updateRestaurantDto);
       return true;
     } catch (e) {
       return false;
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Restaurant } from './entities/restaurants.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 
 @Injectable()
 export class RestaurantService {
@@ -10,13 +11,24 @@ export class RestaurantService {
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
   ) {}
+
   getAll(): Promise<Restaurant[]> {
     return this.restaurants.find();
   }
+
   createRestaurant(
     createRestaurantDto: CreateRestaurantDto,
   ): Promise<Restaurant> {
     const newRestaurant = this.restaurants.create(createRestaurantDto);
     return this.restaurants.save(newRestaurant);
+  }
+
+  updateRestaurant(updateRestaurantoDto: UpdateRestaurantDto) {
+    // console.log('service updateRestaurant id:', updateRestaurantoDto.id);
+    // console.log('service updateRestaurant data:', updateRestaurantoDto.data);
+
+    return this.restaurants.update(updateRestaurantoDto.id, {
+      ...updateRestaurantoDto.data,
+    });
   }
 }
