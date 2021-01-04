64abc909b47ca0d5c706118d6c8d6a8287420bcf
cit007 <itcho0077@gmail.com>
2021-01-04 16:13:52 +0900

    create user : @todo:hash password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -9,4 +10,21 @@ export class UsersService {
     @InjectRepository(User)
     private readonly users: Repository<User>,
   ) {}
+
+  async createAccount(
+    createAccountInput: CreateAccountInput,
+  ): Promise<string | undefined> {
+    const { email, password, role } = createAccountInput;
+    // check new user
+    try {
+      const isMail = await this.users.findOne({ email });
+      if (isMail) {
+        return 'user exists already';
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+    } catch (e) {
+      return 'could not create account';
+    }
+    // TODO : hash password
+  }
 }
