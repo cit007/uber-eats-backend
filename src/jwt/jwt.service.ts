56040240b2574155a7f78b86eef23aad6c74e3e6
cit007 <itcho0077@gmail.com>
2021-01-08 22:10:06 +0900

    jwt option inject test

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtModuleOptions } from './interfaces/jwt-module-options.interface';
 
 @Injectable()
 export class JwtService {
+  constructor(@Inject('JWT_OPT') private readonly options: JwtModuleOptions) {
+    console.log('JWT OPTIONS :', options);
+  }
   hello() {
     console.log('hello JwtService');
   }
