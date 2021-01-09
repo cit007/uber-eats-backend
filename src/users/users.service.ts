823d040f50cb78ec0ccb28cb2a841ec3370e4180
cit007 <itcho0077@gmail.com>
2021-01-10 01:33:00 +0900

    editProfile but password is not hashed. why?

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,6 +6,7 @@ import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from 'src/jwt/jwt.service';
+import { EditProfileInput } from './dtos/edit-profile.dto';
 
 @Injectable()
 export class UsersService {
@@ -61,5 +62,10 @@ export class UsersService {
 
   async findOneById(id: number): Promise<User> {
     return await this.users.findOne({ id });
+  }
+
+  async editProfile(userId: number, editProfileInput: EditProfileInput) {
+    // const { email, password } = editProfileInput; -> @See this is incorrect email or password optional
+    return await this.users.update({ id: userId }, { ...editProfileInput });
   }
 }
