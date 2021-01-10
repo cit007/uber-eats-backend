f2d5bbe897b53450896fef21c7ae1a5c8237718e
cit007 <itcho0077@gmail.com>
2021-01-10 23:35:17 +0900

    verification delete cascade option: if delete user, it also delete verication info with the user

--- src/users/entities/verification.entity.ts
+++ src/users/entities/verification.entity.ts
@@ -12,7 +12,7 @@ export class Verfication extends CoreEntity {
   @Field((type) => String)
   code: string;
 
-  @OneToOne((type) => User)
+  @OneToOne((type) => User, { onDelete: 'CASCADE' })
   @JoinColumn()
   user: User;
 
