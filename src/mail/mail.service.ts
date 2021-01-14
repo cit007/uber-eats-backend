38de54db6b102445d70d0613eae6438bb42914df
cit007 <itcho0077@gmail.com>
2021-01-14 14:35:43 +0900

    mailgun send with template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -15,20 +15,25 @@ export class MailService {
   }
 
   private async sendEmail(subject: string, content: string) {
-    const DOMAIN = this.options.domain;
-    const mg = mailgun({
-      apiKey: this.options.apiKey,
-      domain: DOMAIN,
-    });
-    const data = {
-      from: `Excited User <me@${this.options.domain}/>`,
-      to: 'itcho0077@gmail.com, citack007@gmail.com',
-      subject: subject,
-      text: content,
-    };
-    console.log('sendmail:', data);
-    mg.messages().send(data, function (error, body) {
-      console.log(body);
-    });
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `itcho0077@gmail.com`);
+    form.append('subject', subject);
+    form.append('template', 'verify-email');
+    form.append('v:code', 'asasas');
+    form.append('v:username', 'itcho!!!');
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log(response.body);
   }
 }
